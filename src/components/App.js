adc5f8bff0facfada81d7ccf896430d345f01d01
Joseph Campuzano <jcampuzano@axxess.local>
2017-03-12 20:38:49 -0500

    Making progress

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,60 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+    
+    constructor(props) {
+        super();
+        
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+
+        // initial state
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+
+    }
+
+    addFish(fish) {
+        // update our state
+        // first take a copy of state
+        // then add in new fish
+        const fishes = {...this.state.fishes};
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        
+        // set state
+        this.setState({ fishes: fishes });
+    }
+
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes 
+        });
+    }
+    
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="list-of-fishes">
+                        {Object
+                            .keys(this.state.fishes)
+                            .map(fish => {
+                                return <Fish key={fish} details={this.state.fishes[fish]} />;
+                            })
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
         )
     }
---
+++ src/components/Fish.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+
+class Fish extends React.Component {
+    render() {
+
+        const { details } = this.props;
+
+        return (
+            <li className="menu-fish">
+                <img src={details.image} alt=""/>
+                <h3>
+                    {details.name}
+                    <span className="price">{formatPrice(details.price)}</span>
+                </h3>
+                <p>{details.desc}</p>
+                <button>Add To Order</button>
+            </li>
+        )
+    }
+}
+
+export default Fish;
